4bf5486ff3f47c1e60ed7e9fdb7aa774bd48d03f
Tomer Levy <tl455@factory.cs.rutgers.edu>
2017-01-24 12:10:56 -0500

    Learned some I/O

--- Tomer/C4Dummies.c
+++ Tomer/C4Dummies.c
@@ -6,5 +6,6 @@ int main(int argc, char** argv){
 	scanf("%s" ,&name);
 	printf("Well holy fuck %s go fuck yourself\n" ,name);
 
+	printf("\"To print a string that has quotation marks you needs to use the forward slash followed by the quotation marks. Ending with a slash and quote marks\"\n");
 	return(0);
 }
