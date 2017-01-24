e9280f42e30d7b29d1108cbdbc6d56f3a6b9af08
Tomer Levy <tl455@factory.cs.rutgers.edu>
2017-01-24 12:04:25 -0500

    Learned some I/O

---
+++ Tomer/C4Dummies.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(int argc, char** argv){
+	char name[20];
+
+	printf("What is your name?\n");
+	scanf("%s" ,&name);
+	printf("Well holy fuck %s go fuck yourself\n" ,name);
+
+	return(0);
+}
--- Tomer/ClassShit.c
+++ Tomer/ClassShit.c
@@ -5,7 +5,8 @@ int  main(void){
 	printf("%d, %s\n", wor, wor);
 	char wor2[] = {'h', 'e', 'l', 'l', 'o', '\0'};
 	printf("%d, %s\n", wor2, wor2);
-	printf("Neil is Za Worst");
+	printf("Neil is Za Worst\n");
+	printf("C is actually the worst language\n");
 	return 0;
 }
 
