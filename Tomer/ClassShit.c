ed0b40e38001b65ec664bfb94ed0bcb28d83ba0c
Tomer Levy <tl455@factory.cs.rutgers.edu>
2017-01-24 14:32:44 -0500

    Maybe learned something about Linked Lists????

--- Tomer/ClassShit.c
+++ Tomer/ClassShit.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 #include<string.h>
+struct fNode{
+char* token;
+struct fNode* next;
+}fNode;
+
+typedef struct fNode Node;
+
 int  main(void){
 	char wor[] = {'h', 'e', 'l', 'l', 'o', '\0'};
 	printf("%d, %s\n", wor, wor);
@@ -7,6 +14,13 @@ int  main(void){
 	printf("%d, %s\n", wor2, wor2);
 	printf("Neil is Za Worst\n");
 	printf("C is actually the worst language\n");
+	Node T;	
+	T.token= "Slavery";
+	Node *N;
+	N->token="NeilMalu";
+	T.next=N;
+	printf("%s\n", T.token);
 	return 0;
+	
 }
 
