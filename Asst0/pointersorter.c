5dcb91dfbfe31b023115ba0d1715f802dd2be990
Tomer Levy <tl455@factory.cs.rutgers.edu>
2017-01-26 14:31:56 -0500

    Progress - Added a lot of comments

--- Asst0/pointersorter.c
+++ Asst0/pointersorter.c
@@ -2,7 +2,13 @@
 #include<string.h>
 #include<stdlib.h>
 #include<ctype.h>
-
+/* NEIL: B -> Z -> b ->z for alphabetical order (descending)
+ * Now hes doing some shit where if malloc fails to make an if statement
+ * int* p =(int*)malloc(number*sizeof(int));
+ * if(p==NULL){
+ * 	printf("Malloc failed to allocate on this line...\n");
+ * 	return -1;
+ * }*/
 struct Node{
 	char *componentString;
 	int strLength;
@@ -12,11 +18,13 @@ struct Node{
 void printList(struct Node* head);
 
 int main(int argc, char *argv[]){
-	if(argc != 2){
+	if(argc != 2){ /* Program always ends here, idk what argc!=2 means but the program never gets past here*/
 		printf("Invalid use. Usage string:\t ./pointersorter \"This is valid input\"\n");
+		//printf("Incorrect number of parameters: \n Usage: ./pointersorter \"astring\"\n");
+		//this is some print that explains how to use the program to the user? He wrote it.
 		return 0;
 	}
-	char *input = argv[1];
+	char *input = argv[1]; /* he saying something about dont change argv[1] Why.. he says youre toast*/
 
 	if(input == ""){
 		printf("error\n");
@@ -26,10 +34,10 @@ int main(int argc, char *argv[]){
 
 	struct Node *head = (struct Node*)malloc(sizeof(struct Node));
 	head->componentString = NULL;
-	head->strLength = NULL;
+	head->strLength = 0; //ints cant be null - compiles after this change
 	head->next = NULL;
 	char c;
-	char *word = (char *)malloc(sizeof(char));
+	char *word = (char *)malloc(sizeof(char)); //Says to initialize *word before doing malloc
 	int i = 0, j = 0;
 	for(i = 0; i <= inputLength; i++){
 		c = input[i];
