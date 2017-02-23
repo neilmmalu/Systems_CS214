3bd5b7c6206dfe8dfdc631f4bc4c2dc9d5bf0829
Neil Malu <Neil@Neils-MBP.home>
2017-02-22 19:48:44 -0500

    Fixed header

--- Asst1/mymalloc.c
+++ Asst1/mymalloc.c
@@ -5,13 +5,16 @@ int main(int argc, char *argv[]){
 }
 
 void *initializeBlock(char* myblock){
-	*(short*) myBlock = 4998;
+	short i = 1;
+	*(short*) myblock = 4998;
 	for (i = 1; i < 2500; i++)
 	{
-		*(short*)(myBlock + i) = 0;
+		*(short*)(myblock + i) = 0;
 	}
 }
 
-void *mymalloc(int size){
-	
+void *mymalloc(int size, char *file, int line){
+	if(myblock[0] < 0){
+
+	}
 }
--- Asst1/mymalloc.h
+++ Asst1/mymalloc.h
@@ -1,15 +1,19 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#define malloc(x) mymalloc(x)
-#define free(x) myfree(x)
+#ifndef mymalloc_h
+#define mymalloc_h
+#define malloc(x) mymalloc(x, __FILE__, __LINE__)
+#define free(x) myfree(x, __FILE__, __LINE__)
 
 static char myblock[5000];
 
-void *mymalloc(int size);
+void *mymalloc(int size, char *file, int line);
 
-void *myfree(char *p);
+void *myfree(char *p, char *file, int line);
 
 int cleanUp(char* myblock, int size);
 
 void *initializeBlock(char* myblock);
+
+#endif
