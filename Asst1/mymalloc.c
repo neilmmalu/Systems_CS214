9eaa9b2c4aaa200b7a02928a98a477b6fad2516e
Neil Malu <Neil@Neils-MBP.home>
2017-02-25 19:14:40 -0500

    AllocateMem

--- Asst1/mymalloc.c
+++ Asst1/mymalloc.c
@@ -3,17 +3,12 @@
 int isEmpty = 1;
 
 int main(int argc, char *argv[]){
-	initializeBlock();
-	int i = 0;
-	for(i = 0; i < 5000; i++){
-		printf("%c", myblock[i]);
-	}
 }
 
 void initializeBlock(){
 	char *p;
     p = &myblock[0];
-    *((short*)p) = 4998;
+    *((short*)p) = -5000;
 }
 
 void *mymalloc(int size, char *file, int line){
@@ -31,17 +26,47 @@ void *mymalloc(int size, char *file, int line){
 }
 
 void *allocateMem(int size){
-	if(size < 1 || size > 5000){
-		return NULL;
-	}
-	if(size > 4998 && size < 5000){
-		size = 4998;
-	}
 	char *temp = myblock;
-	int newSize = size + 2;
-	int index = 0;
+    int newSize = size + 2;
+    int index = 0;
+    
+    int i;
+    i = 0;
 
-	// do{
+    //too small or too big
+    if (size < 1 || size > 4998){
+    	return NULL;
+    }
 
-	// }while(index < 5000);
+    do {
+
+        short *mallocSize = (short*)(temp);
+        short mallocSizeMetadata = *mallocSize * -1;
+        if (mallocSizeMetadata < 0){
+            mallocSizeMetadata *= -1;
+        }
+
+        int margin = mallocSizeMetadata - newSize;
+        if((*mallocSize < 0) && (margin == 0 || margin > 2)){
+            
+        	short *p;
+            //sets size of allocated space
+            p = (short*)temp;
+            *p = newSize;
+            
+            //sets size of new unallocated block if block is split
+            if (newSize != mallocSizeMetadata){
+                p = (short*)(temp + newSize);
+                *p = (mallocSizeMetadata - newSize) * -1; //negative because unallocated
+            }
+            return (temp + 2);
+        }
+        else{
+            index = index + mallocSizeMetadata;
+            temp = &myblock[index];
+            i++;
+        }
+    }while (index < 5000);
+
+    return NULL;
 }
