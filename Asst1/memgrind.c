9b0422a646c594c01afa3f4dd50a53a91b493bd8
Tomer Levy <tl455@factory.cs.rutgers.edu>
2017-02-25 19:21:28 -0500

    memgrind

--- Asst1/memgrind.c
+++ Asst1/memgrind.c
@@ -1,60 +1,72 @@
 //#include "mymalloc.c"
 #include<stdlib.h>
 #include<stdio.h>
+void *partA(char *test){
+	int i=0;
+	for(i=0;i<1000;i++){
+		test[i] = (char*)malloc(sizeof(char));
+	}
+	for(i=0;i<1000;i++){
+		free(test[i]);
+	}
+	return;
+}
+void *partB(char *test){
+	int i=0;
+	for(i=0;i<1000;i++){
+		test[i]=(char*)malloc(sizeof(char));
+		free(test[i]);
+	}
+	return;
+}
+void *partC(char *test){
+	int k=0;
+	while(k<1000){
+		int r =rand() %2;
+		switch(r){
+			case 0:
+				test[k] = (char*)malloc(sizeof(char));
+				k++;
+				break;
+			case 1:
+				if(k!=0){
+				free(test[k-1]);
+				}else{
+				//freeing first empty block (error)
+				}
+				break;
+			default:
+				break;
+			}
+		}
+	free(test);
+	return;
+}
+void *partD(char *test){
+	int k=0;
+	while(k<1000){
+		int r=rand() %2;
+		switch(r){
+			case 0:
+				int bytes = (rand() %64) +1;
+				k+=bytes;
+				break;
+			case 1:
+				//free first pointer it finds
+				break;
+			default:
+				break;
+			}
+	}
+	free(test);
+	return;
+}
 int main(int argc, char** argv){
 	char *test[1000];
-	// Part A
-	int i=0;
-	for(i=0;i<1000;i++){
-		test[i] = (char*)malloc(sizeof(char));
-	}
-	for(i=0;i<1000;i++){
-		free(test[i]);
-	}
-	//Part B
-	int i=0;
-	for(i=0;i<1000;i++){
-		test[i] = (char*)malloc(sizeof(char));
-		free(test[i]);
-	}
-	// Part C
-	int k=0;
-	while(k<1000){
-		int r = rand() %2;
-	switch(r){
-		case 0:
-			test[k] = (char*)malloc(sizeof(char));
-			k++;
-			break;
-		case 1:
-			if(k!=0){
-			free(test[k-1]);
-			}
-			break;
-		default:
-			break;
-	}
-		free(test);
-	}
-	// Part D
-	int k=0;
-	while(k<1000){
-		int r = rand() %2;
-	switch(r){
-		case 0:
-			int bytes = rand() %65;
-			break;
-			//check if space is not being used already
-			//test[somenumber] = malloc(bytes) amount of bytes
-		case 1:
-			//free the first pointer it can find
-			break;
-		default:
-			break;
-	}
-	
-
-
+	partA(test);
+	partB(test);
+	partC(test);
+	partD(test);
 
 	return 0;
 	}
