d11b210dbe25ab2544fe903919874e7c7ea01ea8
Tomer Levy <tl455@factory.cs.rutgers.edu>
2017-02-26 21:41:29 -0500

    help

--- Asst1/memgrind.c
+++ Asst1/memgrind.c
@@ -8,6 +8,8 @@ void partA(char *test[1000]);
 void partB(char *test[1000]);
 void partC(char *test[1000]);
 void partD(char *test[1000]);
+void PartE(char *test[1000]);
+void PartF(char *test[1000]);
 void endGrind();
 
 void partA(char *test[1000]){
@@ -62,7 +64,7 @@ void partB(char *test[1000]){
 void partC(char *test[1000]){
 	struct timeval beginTime;
 	struct timeval endTime;
-	
+	long average =0;
 	int k=0;
 	time_t t;
 	srand((unsigned) time(&t));
@@ -90,11 +92,12 @@ void partC(char *test[1000]){
 					break;
 			}
 		}
-	}
-//	free(test);
-	long average=0;
+	
+free(test);
+	
 	gettimeofday(&endTime,0);
 	average += ((endTime.tv_sec-beginTime.tv_sec)*1000000 + endTime.tv_usec-beginTime.tv_usec);
+	}
 	printf("Grind C time: %ld\n",average/100); 
 	endGrind();
 	return;
@@ -111,16 +114,18 @@ void partD(char *test[1000]){
 		int k=0;
 		while(k<1000){
 			int r=rand() %2;
-			int bytes;
+			int bytes=0;
 			switch(r){
 				case 0:
 					
 					bytes = rand()%64;
 					bytes+=1;
-					k+=bytes;
+					test[k]=(char*)malloc(bytes*sizeof(char));
+					k++;
+					
 					break;
 				case 1:
-					//free first pointer it finds
+					free(test[k]);
 					break;
 				default:
 					break;
@@ -133,13 +138,40 @@ void partD(char *test[1000]){
 	printf("%ld\n",average/100); 
 	return;
 }
+void partE(char *test[1000]){
+	struct timeval beginTime;
+	struct timeval endTime;
+	int x=0;
+	long average=0;
+	time_t t;
+	srand((unsigned) time(&t));
+	for(x=0;x<100;x++){
+		gettimeofday(&beginTime,0);
+		int k=0;
+		while(k<1000){
+			test[k]=(char*)malloc(4996-k*2);
+			numMallocs++;
+			free(test[k]);
+			numFrees++;
+			k++;
+		}
+	
+	
+	gettimeofday(&endTime,0);
+	average +=((endTime.tv_sec-beginTime.tv_sec)*1000000 + endTime.tv_usec-beginTime.tv_usec);
+	}
+	
+	endGrind();
+}	
+
 int main(int argc, char** argv){
 	char *test[1000];
 	partA(test);
 	partB(test);
 	partC(test);
-//	partD(test);
-
+//partD(test);
+	partE(test);
+//	partF(test);
 	return 0;
 	}
 
