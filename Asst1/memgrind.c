d1a9136cb07ea4161815493dfb47f8228cff3a67
Neil Malu <Neil@Neils-MBP.home>
2017-02-26 20:50:53 -0500

    Second grind successful

--- Asst1/memgrind.c
+++ Asst1/memgrind.c
@@ -1,13 +1,14 @@
 #include "mymalloc.c"
 #include<time.h>
 
-// long numMallocs = 0;
-// long numFrees = 0;
+int numMallocs = 0;
+int numFrees = 0;
 
 void partA(char *test[1000]);
 void partB(char *test[1000]);
 void partC(char *test[1000]);
 void partD(char *test[1000]);
+void endGrind();
 
 void partA(char *test[1000]){
 	struct timeval beginTime;
@@ -20,16 +21,19 @@ void partA(char *test[1000]){
 		int i=0;
 		for(i=0;i<1000;i++){
 			test[i] = (char*)malloc(sizeof(char));
+			numMallocs++;
 			// printf("numMallocs : %li\n",numMallocs++);
 		}
 		for(i=0;i<1000;i++){
 			free(test[i]);
+			numFrees++;
 			// printf("numFrees: %li\n", numFrees++);
 		}
 		gettimeofday(&endTime,0);
 		average += ((endTime.tv_sec-beginTime.tv_sec)*1000000 + endTime.tv_usec-beginTime.tv_usec);
 	}
-	printf("%ld\n",average/100); 
+	printf("Grind A time: %ld\n",average/100); 
+	endGrind();
 	return;
 }
 void partB(char *test[1000]){
@@ -43,13 +47,16 @@ void partB(char *test[1000]){
 		int i=0;
 		for(i=0;i<1000;i++){
 			test[i]=(char*)malloc(sizeof(char));
-		//	free(test[i]);
+			numMallocs++;
+			free(test[i]);
+			numFrees++;
 		}
 		gettimeofday(&endTime,0);
 		average += ((endTime.tv_sec-beginTime.tv_sec)*1000000 + endTime.tv_usec-beginTime.tv_usec);
 	}
 
-	printf("%ld\n",average/100); 
+	printf("Grind B time: %ld\n",average/100); 
+	endGrind();
 	return;
 }
 void partC(char *test[1000]){
@@ -121,10 +128,17 @@ void partD(char *test[1000]){
 int main(int argc, char** argv){
 	char *test[1000];
 	partA(test);
-//	partB(test);
+	partB(test);
 //	partC(test);
 //	partD(test);
 
 	return 0;
 	}
 
+void endGrind(){
+	printf("Number of mallocs: %d\n", numMallocs);
+	printf("Number of frees: %d\n", numFrees);
+	numFrees = 0;
+	numMallocs = 0;
+	printf("\n");
+}
