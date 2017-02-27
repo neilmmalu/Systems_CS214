1408a89d0755607ce4a39ab9836f5937d3b99aed
Neil Malu <Neil@Neils-MBP.home>
2017-02-26 21:05:33 -0500

    We're fucking killing this shit

--- Asst1/memgrind.c
+++ Asst1/memgrind.c
@@ -62,33 +62,41 @@ void partB(char *test[1000]){
 void partC(char *test[1000]){
 	struct timeval beginTime;
 	struct timeval endTime;
-	gettimeofday(&beginTime,0);
+	
 	int k=0;
 	time_t t;
 	srand((unsigned) time(&t));
-	while(k<1000){
-		int r =rand() %2;
-		switch(r){
-			case 0:
-				test[k] = (char*)malloc(sizeof(char));
-				k++;
-				break;
-			case 1:
-				if(k!=0){
-			//	free(test[k-1]);
-				}else{
-				//freeing first empty block (error)
-				}
-				break;
-			default:
-				break;
+	int x = 0;
+	gettimeofday(&beginTime,0);
+	for(x = 0; x < 100; x++){
+		while(k<1000){
+			int r =rand() %2;
+			switch(r){
+				case 0:
+					test[k] = (char*)malloc(sizeof(char));
+					k++;
+					numMallocs++;
+					break;
+				case 1:
+					if(k!=0){
+						free(test[k-1]);
+						numFrees++;
+					}else{
+						free(test[0]);
+						numFrees++;
+					}
+					break;
+				default:
+					break;
 			}
 		}
+	}
 //	free(test);
 	long average=0;
 	gettimeofday(&endTime,0);
 	average += ((endTime.tv_sec-beginTime.tv_sec)*1000000 + endTime.tv_usec-beginTime.tv_usec);
-	printf("%ld\n",average/100); 
+	printf("Grind C time: %ld\n",average/100); 
+	endGrind();
 	return;
 }
 void partD(char *test[1000]){
@@ -129,7 +137,7 @@ int main(int argc, char** argv){
 	char *test[1000];
 	partA(test);
 	partB(test);
-//	partC(test);
+	partC(test);
 //	partD(test);
 
 	return 0;
