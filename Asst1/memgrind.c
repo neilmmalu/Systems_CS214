9557cc6eead55d8cccd1cc06a5f19e2a14799880
Neil Malu <Neil@Neils-MBP.home>
2017-02-26 20:40:32 -0500

    Test 1 successful

--- Asst1/memgrind.c
+++ Asst1/memgrind.c
@@ -1,8 +1,8 @@
 #include "mymalloc.c"
 #include<time.h>
 
-long numMallocs = 0;
-long numFrees = 0;
+// long numMallocs = 0;
+// long numFrees = 0;
 
 void partA(char *test[1000]);
 void partB(char *test[1000]);
@@ -20,11 +20,11 @@ void partA(char *test[1000]){
 		int i=0;
 		for(i=0;i<1000;i++){
 			test[i] = (char*)malloc(sizeof(char));
-			printf("numMallocs : %li\n",numMallocs++);
+			// printf("numMallocs : %li\n",numMallocs++);
 		}
 		for(i=0;i<1000;i++){
 			free(test[i]);
-			printf("numFrees: %li\n", numFrees++);
+			// printf("numFrees: %li\n", numFrees++);
 		}
 		gettimeofday(&endTime,0);
 		average += ((endTime.tv_sec-beginTime.tv_sec)*1000000 + endTime.tv_usec-beginTime.tv_usec);
