13aa119cbcf8b848409647cdd3e78cededcd7bdc
Neil Malu <Neil@Neils-MBP.home>
2017-03-29 20:43:44 -0400

    Test

--- Asst2/tokenizer.c
+++ Asst2/tokenizer.c
@@ -508,23 +508,23 @@ void insertNode(Node* head, hashTable* list , char* file)
 }
 
 
-int main(int argc, char** argv)
-{
-	hashTable* mainTable = createHashTable(36);
+// int main(int argc, char** argv)
+// {
+// 	hashTable* mainTable = createHashTable(36);
 
-	if(argc!=3)
-    {
-	  	printf("usage: ./index output_file target_file/directory \n");
-        return 1;
-    }
-	if (exists(argv) == 0)
-	{
-		return 0;
-	}
-	traverseDirectory(mainTable, argv[2]);
-	FILE* mainOutputFile = fopen(argv[1], "w+");
-	outputTokens(mainTable, mainOutputFile);
-	return 0;
-}
+// 	if(argc!=3)
+//     {
+// 	  	printf("usage: ./index output_file target_file/directory \n");
+//         return 1;
+//     }
+// 	if (exists(argv) == 0)
+// 	{
+// 		return 0;
+// 	}
+// 	traverseDirectory(mainTable, argv[2]);
+// 	FILE* mainOutputFile = fopen(argv[1], "w+");
+// 	outputTokens(mainTable, mainOutputFile);
+// 	return 0;
+// }
 
 
