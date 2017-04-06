b7391a0e15b307c3f32b48615616680005ae518d
Neil Malu <Neil@Neils-MBP.home>
2017-04-06 17:08:31 -0400

    Yo

---
+++ Asst2_5/part1_functions.h
@@ -0,0 +1,72 @@
+<<<<<<< HEAD
+
+
+#ifndef __PART_1_FUNCTIONS_H__
+#define __PART_1_FUNCTIONS_H__
+
+void one(const int a, const int b);
+void two(const char *grade);
+void three();
+void four(const float value);
+void five(const int *x, const int *y);
+float *six(const int *x);
+void seven(const char *a);
+void eight();
+void nine();
+void ten(int *x);
+void eleven(const char *s);
+void twelve();
+void thirteen(int a);
+void fourteen(const char *s);
+void fifteen(const int value);
+char *sixteen();
+void seventeen(const int d);
+void eighteen(int k);
+long int clear_bits(long int value, long int flag);
+
+#endif // __PART_1_FUNCTIONS_H__
+=======
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+	<head>
+	    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	    <link href="/library/skin/tool_base.css" type="text/css" rel="stylesheet" media="all" />
+	    <link href="/library/skin/morpheus-default/tool.css" type="text/css" rel="stylesheet" media="all" />
+	    <meta http-equiv="Content-Style-Type" content="text/css" />
+	    <title>sakai.rutgers.edu</title>
+	    <script type="text/javascript" language="JavaScript" src="/library/js/headscripts.js"></script>
+		<meta name="viewport" content="width=device-width"/>
+	</head>
+	<body>
+		
+		<!-- choice.vm -->
+		<table id="login-choice" class="login">
+			<tr>
+    			<th id="login-choice-header" colspan="2">Select your authentication source</th>
+    		</tr>
+    		<tr>
+    			<td class="form" id="login-choice-container">
+					<p>
+						<a href="https://content.sakai.rutgers.edu/sakai-login-tool/container">
+    						    						NetID Login
+    					</a>
+					</p>
+					
+				</td>
+				
+				<td class="form" id="login-choice-xlogin">
+					<p>
+						<a href="https://content.sakai.rutgers.edu/portal/xlogin">
+    						    						Email Login
+						</a>
+					</p>
+					
+				</td>
+    		</tr>
+		</table>
+		<!-- end choice.vm -->
+				
+	</body>
+</html>
+>>>>>>> 303b008e3cd852f97a9719dfd238396fc2756680
