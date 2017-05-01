2b665b59a5b632a162500675f1fe4bc2240ae7ac
Neil Malu <Neil@nwk-141-209.nwk.ruw.rutgers.edu>
2017-05-01 19:30:45 -0400

    Moment of truth

--- Asst3/libnetfiles.c
+++ Asst3/libnetfiles.c
@@ -1,29 +1,20 @@
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <netdb.h>
-#include <string.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/socket.h>
 #include "libnetfiles.h"
 
 typedef struct {
 	char hostname[64];
-	CONNECTION_MODE fMode;
+	CONNECTION_MODE conn_mode;
 } SERVER_CONN;
 
-SERVER_CONN clientConn;
+SERVER_CONN CONNECTION;
 
-int getSocket(const char *hostname){
-	int sockfd = 0;
+int socketGen(const char *hostname){
+	int socket_fd = 0;
 
-	struct sockaddr_in serv_addr;
+	struct sockaddr_in server_address;
 	struct hostent *server = NULL;
 
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if(sockfd < 0){
+	socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if(socket_fd < 0){
 		fprintf(stderr, "libnetfiles: socket() failed errno = %d\n", errno);
 		return -1;
 	}
@@ -36,5 +27,303 @@ int getSocket(const char *hostname){
 		return -1;
 	}
 
-	
+	memset((char *) &server_address, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    memcpy((char *)server->h_addr, (char *)&server_address.sin_addr.s_addr, server->h_length);
+    server_address.sin_port = htons(PORT_NO);
+
+    if(connect(socket_fd,(struct sockaddr *)&server_address, sizeof(server_address)) < 0){
+        fprintf(stderr,"libnetfiles: cannot connect to %s, h_errno= %d\n", hostname, h_errno);
+    	return -1;
+    }
+
+    return socket_fd;
 }
+
+extern int netserverinit(char *hostname, int filemode){
+	
+	errno =  0;		
+    h_errno = 0;
+	int serv_ret = 0;
+    int socket_fd = -1;
+    char buff[BUFF_SIZE] = "";
+
+    strcpy(CONNECTION.hostname, buff);													
+	CONNECTION.conn_mode = INVALID_FILE;
+
+	switch(filemode){
+        case UNRESTRICTED:
+        case EXCLUSIVE:   
+        case TRANSACTION:   
+            break;
+
+        default:
+            h_errno = INVALID_FILE;
+            fprintf(stderr, "netserverinit: invalid file connection mode\n");
+            return -1;
+    }
+
+    if(hostname == NULL || strlen(hostname) == 0){
+        errno = EINVAL;  
+        return -1;
+    }
+
+    socket_fd = socketGen(hostname);
+    if(socket_fd < 0){
+        errno = 0;
+       	h_errno = HOST_NOT_FOUND;
+        return -1;
+    }
+
+    memset(buff, BUFF_SIZE);
+    sprintf(buff, "%d,0,0,0", 1);
+
+    serv_ret = write(socket_fd, buff, strlen(buff));
+    if(serv_ret < 0){
+       	h_errno = ECOMM;  
+        printf("Failed to write to SERVER_CONN");
+        return -1;
+    }
+
+    memset(buff, BUFF_SIZE);
+    serv_ret = read(socket_fd, buff, BUFF_SIZE - 1);  
+
+    if(serv_ret < 0){
+        h_errno = ECOMM;  
+        printf("Failed to read from socket\n");
+        if(socket_fd != 0) 
+        	close(socket_fd);
+        return -1;
+    }
+
+    close(socket_fd);
+
+    sscanf(buff, "%d,", &serv_ret);
+    if(serv_ret == 0){
+    	strcpy(CONNECTION.hostname, hostname);
+        CONNECTION.conn_mode = (CONNECTION_MODE)filemode;
+    }
+
+    return serv_ret;
+}
+
+int netopen(const char *pathname, int flags){
+	
+	errno = 0;
+    h_errno = 0;
+	int net_addr  = -1;
+    int socket_fd = -1;
+    int serv_ret = 0;
+    char buff[BUFF_SIZE] = "";
+
+    if(pathname == NULL || strlen(pathname) < 1){
+        printf("Pathname is Invalid\n");
+        errno = EINVAL;  
+        return -1;
+    }
+
+    if(flags != O_RDONLY && flags != O_WRONLY && flags != O_RDWR){
+		printf("Invalid flags argument\n");
+		errno = EINVAL;
+		return -1;
+    }
+
+    socket_fd = socketGen(CONNECTION.hostname);
+    if(socket_fd < 0){
+        errno = 0;
+        h_errno = HOST_NOT_FOUND;
+        fprintf(stderr, "netopen: host not found, %s\n", CONNECTION.hostname);
+        return -1;
+    }
+
+    memset(buff, BUFF_SIZE);
+    sprintf(buff, "%d,%d,%d,%s", NET_OPEN, CONNECTION.conn_mode, flags, pathname);
+
+    serv_ret = write(socket_fd, buff, strlen(buff));
+    if(serv_ret < 0){
+        fprintf(stderr, "netopen: failed to write cmd to CONNECTION.  serv_ret= %d\n", serv_ret);
+        return -1;
+    }
+
+	memset(buff, BUFF_SIZE);
+	serv_ret = read(socket_fd, buff, BUFF_SIZE - 1);
+
+	if(serv_ret < 0){
+        if(socket_fd != 0)
+        	close(socket_fd);
+        return -1;
+    }
+
+    close(socket_fd);    
+
+    sscanf(buff, "%d,%d,%d,%d", &serv_ret, &net_addr, &errno, &h_errno);
+
+    if(serv_ret == -1){
+        printf("netopen: SERVER_CONN returns FAIL, errno= %d (%s), h_errno=%d\n", errno, strerror(errno), h_errno);
+        return -1;
+    }
+
+    return net_addr;
+}
+
+extern int netclose(int fd){
+	if(fd >= 0){
+        errno = EBADF;
+        return -1;
+    }
+
+    errno = 0;
+    h_errno = 0;
+
+    int net_addr  = -1;
+    int socket_fd = -1;
+    int serv_ret = 0;
+    char buff[BUFF_SIZE] = "";
+
+
+    socket_fd = socketGen(CONNECTION.hostname);
+    if(socket_fd < 0){
+        errno = 0;
+        h_errno = HOST_NOT_FOUND;
+        fprintf(stderr, "netopen: host not found, %s\n", CONNECTION.hostname);
+        return -1;
+    }
+
+    memset(buff, BUFF_SIZE);
+    sprintf(buff, "%d,%d", NET_CLOSE, fd);
+
+    serv_ret = write(socket_fd, buff, strlen(buff));
+
+    if(serv_ret < 0){
+        h_errno = ECOMM;  
+        printf("Failed to write to SERVER_CONN");
+        return -1;
+    }
+
+    memset(buff, BUFF_SIZE);
+    serv_ret = read(socket_fd, buff, BUFF_SIZE - 1);
+
+    if(serv_ret < 0){
+        h_errno = ECOMM;  
+        printf("Failed to read from socket\n");
+        if(socket_fd != 0)
+        	close(socket_fd);
+        return -1;
+    }
+
+    close(socket_fd); 
+    sscanf(buff, "%d,%d,%d", &serv_ret, &errno, &h_errno);  
+    return serv_ret; 
+}
+
+extern ssize_t netread(int fildes, void *buf, size_t nbyte){
+	if(fildes >= 0){
+        errno = EBADF;
+        return -1;
+    }
+
+    errno = 0;
+    h_errno = 0;
+
+	int net_addr  = -1;
+    int socket_fd = -1;
+    int serv_ret = 0;
+    char * temp_buffer = (char *)buf;
+    char buff[BUFF_SIZE];
+    
+
+    socket_fd = socketGen(CONNECTION.hostname);
+    if(socket_fd < 0){
+        errno = 0;
+        h_errno = HOST_NOT_FOUND;
+        fprintf(stderr, "netopen: host not found, %s\n", CONNECTION.hostname);
+        return -1;
+    }
+
+	memset(buff, BUFF_SIZE); 
+	sprintf(buff, "%u,%d,%d", NET_READ, fildes, (int)nbyte);
+
+    serv_ret = write(socket_fd, buff, strlen(buff));
+
+    if(serv_ret < 0){
+        h_errno = ECOMM;  
+        printf("Failed to write to SERVER_CONN");
+        return -1;
+    }   
+
+	memset(buff, BUFF_SIZE); 
+	serv_ret = read(socket_fd, buff, BUFF_SIZE - 1);   
+
+	if(serv_ret < 0){
+        h_errno = ECOMM;  
+        printf("Failed to read from socket\n");
+        if(socket_fd != 0)
+        	close(socket_fd);
+        return -1;
+    }
+
+    close(socket_fd); 
+
+    sscanf(buff, "%d,%d,%d", &serv_ret, &net_addr, &h_errno);  
+    strncpy(temp_buffer, buff+strlen(buff)-nbyte, nbyte);
+
+    if(serv_ret == -1) 
+    	return -1;
+
+    return net_addr;
+}
+
+extern ssize_t netwrite(int fildes, const void *buf, size_t nbyte){
+
+	errno = 0;
+    h_errno = 0;
+	int net_addr  = -1;
+    int socket_fd = -1;
+    int serv_ret = 0;
+    char * temp_buffer = (char *)buf;
+    char buff[BUFF_SIZE];
+    
+    if(fildes >= 0){
+        errno = EBADF;
+        return -1;
+    }
+
+    socket_fd = socketGen(CONNECTION.hostname);
+    if(socket_fd < 0){
+        errno = 0;
+        h_errno = HOST_NOT_FOUND;
+        fprintf(stderr, "netopen: host not found, %s\n", CONNECTION.hostname);
+        return -1;
+    }
+
+	memset(buff, BUFF_SIZE); 
+	sprintf(buff, "%u,%d,%d,%d,%s", NET_WRITE, fildes, (int)nbyte, strlen(temp_buffer), temp_buffer);
+
+    serv_ret = write(socket_fd, buff, strlen(buff));
+
+    if(serv_ret < 0){
+        h_errno = ECOMM;  
+        printf("Failed to write to SERVER_CONN");
+        return -1;
+    }    
+
+	memset(buff, BUFF_SIZE);
+	serv_ret = read(socket_fd, buff, BUFF_SIZE - 1);
+	if(serv_ret < 0){
+        h_errno = ECOMM;  
+        printf("Failed to read from socket\n");
+        if(socket_fd != 0)
+        	close(socket_fd);
+        return -1;
+    }
+
+    close(socket_fd); 
+
+    sscanf(buff, "%d,%d,%d", &serv_ret, &net_addr, &errno);  
+    if(serv_ret == -1)
+    	return -1;
+   
+    return net_addr;     
+}
+
+
---
+++ Asst3/netfileserver.c
@@ -0,0 +1,329 @@
+#include "libnetfiles.h"
+
+typedef struct{
+    int  server_fd;
+    int local_fd;
+    CONNECTION_MODE conn_mode;
+    int flag;
+    char file_pwd[256];
+}file_data;
+
+static int check = 0;
+
+file_data data_table[DATA_TABLE_SIZE];
+
+
+
+int main(int argc, char *argv[]){
+    int socket_fd = 0;
+    int newsocket_fd = 0;
+    pthread_t thread_id = 0;
+
+    struct sockaddr_in server_address, client_address;
+    int client_address_length = sizeof(client_address);
+    createTable();
+ 	socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_fd < 0) {
+        fprintf(stderr,"netfileserver: socket() failed, errno= %d\n", errno);
+        exit(EXIT_FAILURE);
+    }
+
+	memset((char *) &server_address, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port = htons(PORT_NO);
+    if (bind(socket_fd, (struct sockaddr *) &server_address, sizeof(server_address)) < 0){
+        fprintf(stderr,"netfileserver: bind() failed, errno= %d\n", errno);
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(socket_fd, 50) < 0){
+        fprintf(stderr,"netfileserver: listen() failed, errno= %d\n", errno);
+        close(socket_fd);
+        exit(EXIT_FAILURE);
+    }
+    // else{}
+
+
+while(check == 0){
+        printf("netfileserver: listener is waiting to accept incoming request\n");
+        if ((newsocket_fd = accept(socket_fd, (struct sockaddr *)&client_address, &client_address_length)) < 0){
+            if (errno != EINTR){
+                fprintf(stderr,"netfileserver: accept() failed, errno= %d\n", errno);
+            	close(newsocket_fd);
+            	if(socket_fd != 0)
+            		close(socket_fd);
+                exit(EXIT_FAILURE);
+            }
+            else{
+            	close(newsocket_fd);
+            	if(socket_fd != 0)
+            		close(socket_fd);
+                check = 1;  
+            }
+        }
+        else{
+            printf("netfileserver: listener accepted a new request from socket\n");
+            pthread_create(&thread_id, NULL, &thread_process, &newsocket_fd );
+            printf("netfileserver: listener spawned a new worker thread with ID Thread : %lu\n",thread_id);
+        }
+    }
+
+    if(newsocket_fd != 0)
+    	close(newsocket_fd);
+    if(socket_fd != 0 )
+    	close(socket_fd);
+
+    printf("netfileserver: terminated\n");
+    return 0;
+}
+
+void *thread_process(void *socket){
+    int *socket_fd = socket;
+    file_data *newFd = NULL;
+    int wbuff_length = 0;
+    int temp = 0;
+    int file_descriptor = -1;
+    char buff[BUFF_SIZE] = "";
+    NET_FUNCTION_TYPE function_type = INVALID;
+
+    temp = pthread_detach(pthread_self());
+    temp = read(*socket_fd, buff, BUFF_SIZE -1);
+
+    if ( temp < 0 ){
+        fprintf(stderr,"Thread: %lu failed to read from socket\n", pthread_self());
+        if(*socket_fd != 0)
+        	close(*socket_fd);
+		pthread_exit(NULL);
+    }
+    else{
+        printf("Thread: %lu received \"%s\"\n", pthread_self(), buff);
+    }
+
+    int * nbyte = malloc(sizeof(int));
+    char readBuffer [500];
+    sscanf(buff, "%u,", &function_type);
+   
+    switch(function_type){
+
+        case NET_SERVERINIT:
+            sprintf(buff, "%d,0,0,0", 0);
+            printf("Thread : %lu responding with \"%s\"\n", pthread_self(), buff);
+            break;
+
+        case NET_OPEN:
+            newFd = malloc(sizeof(file_data));
+
+            sscanf(buff, "%u,%d,%d,%s", &function_type, (int *)&(newFd->conn_mode), &(newFd->flag), newFd->file_pwd);
+
+            temp = open(newFd);
+            if (temp == -1){
+				memset(buff, BUFF_SIZE);
+                sprintf(buff, "%d,%d,%d,%d", -1, temp, errno, h_errno);
+            } 
+            else {
+				memset(buff, BUFF_SIZE);
+                sprintf(buff, "%d,%d,%d,%d", 0, temp, errno, h_errno);
+            }
+            printf("Thread : %lu responding with \"%s\"\n", pthread_self(), buff);
+            free(newFd);
+            break;
+
+        case NET_READ:
+            sscanf(buff, "%u,%d, %d", &function_type, &file_descriptor, nbyte);
+            temp = read(file_descriptor, *nbyte, readBuffer);
+            if(temp==-1){
+                sprintf(buff, "%d,%d,%d,%d", -1, errno, h_errno, temp);
+            }
+            else{
+                readBuffer[temp]='\0';
+                sprintf(buff, "%d,%d,%d,%s", 0, temp, errno, readBuffer);
+            }
+	    	free(nbyte);
+            break;
+
+        case NET_WRITE: 
+            sscanf(buff, "%u,%d,%d,%d", &function_type, &file_descriptor, nbyte, &wbuff_length);
+	    	strncpy(readBuffer, buff+strlen(buff)-wbuff_length, *nbyte);
+            temp = write(file_descriptor, readBuffer, *nbyte);
+            if(temp == -1){
+                sprintf(buff, "%d,%d,%d", -1, errno, h_errno);
+                printf("write Failed with %d\n", temp);
+            }
+            else{
+                sprintf(buff, "%d,%d,%d", 0, temp, errno);
+                printf("write succeeded with %d\n", temp);
+            }
+	    	free(nbyte);
+            break;
+
+        case NET_CLOSE:
+            sscanf(buff, "%u,%d", &function_type, &file_descriptor);
+            temp = deleteFD(file_descriptor);                               
+            if(temp == -1){
+                sprintf(buff, "%d,%d,%d,%d", -1, errno, h_errno, temp);
+            } 
+            else{
+                sprintf(buff, "%d,%d,%d,%d", 0, errno, h_errno, temp);
+            }
+            break;
+
+        case INVALID:
+            break;
+
+        default:
+            printf("Thread : %lu received invalid net function\n", pthread_self());
+            break;
+
+	}    
+	temp = write(*socket_fd, buff, (strlen(buff)+1));                                                // Send Server response back to client
+    //printf("value of n: %d\n", n);
+    if(temp < 0){
+        fprintf(stderr,"Thread : %lu fails to write to socket\n", pthread_self());
+    }
+    
+    pthread_exit(NULL);
+}
+
+
+int open(file_data *new){
+    int res = -1;
+    if(canOpen(new) == 0){
+        return -1;
+    }
+    new->local_fd = open(new->file_pwd, new->flag);
+    if(new->local_fd < 0)
+    	return -1;
+    res = createFD(new);
+    if(res == -1){
+        errno = ENFILE;
+        return -1;
+    } 
+    return res;  
+}
+
+
+int canOpen(file_data *new){
+    int i;
+    for(i=0; i < DATA_TABLE_SIZE; i++){
+
+        if (strcmp(data_table[i].file_pwd, new->file_pwd) == 0){
+            if(data_table[i].conn_mode == TRANSACTION || new->conn_mode == TRANSACTION){
+				return 0;
+	    	}
+
+	    	if(data_table[i].conn_mode == UNRESTRICTED && (data_table[i].flag == O_WRONLY || data_table[i].flag == O_RDWR)){
+				if(new->conn_mode == EXCLUSIVE && (new->flag == O_WRONLY || new->flag == O_RDWR)){
+					return 0;
+				}
+	    	}
+
+	    	if(data_table[i].conn_mode == EXCLUSIVE && (data_table[i].flag == O_WRONLY || data_table[i].flag == O_RDWR)){
+				if(new->conn_mode == UNRESTRICTED && (new->flag == O_WRONLY || new->flag == O_RDWR)){
+					return 0;
+				}
+
+				if(new->conn_mode == EXCLUSIVE && (new->flag == O_WRONLY || new->flag == O_RDWR)){
+					return 0;
+				}
+	    	}
+        }
+    }
+    return 1;
+}
+
+
+void createTable(){
+    int i = 0;
+    for (i=0; i < DATA_TABLE_SIZE; i++) {
+        data_table[i].local_fd = 0;
+        data_table[i].server_fd = 0; 
+        data_table[i].conn_mode = INVALID_FILE;        
+        data_table[i].flag = O_RDONLY;        
+        data_table[i].file_pwd[0] = '\0';        
+    }
+}
+
+
+int createFD(file_data *new){
+    int i = 0;
+    int n = -1;
+
+    for (i=0; i < DATA_TABLE_SIZE; i++) {
+        if (data_table[i].local_fd == 0 ){
+            data_table[i].local_fd = new->local_fd;
+            data_table[i].server_fd = (-10 * (i + 5));
+            data_table[i].conn_mode = new->conn_mode;        
+            data_table[i].flag = new->flag;        
+            strcpy(data_table[i].file_pwd, new->file_pwd);
+            return data_table[i].server_fd;  
+         }
+    }
+    return -1;                                                                           
+}
+
+int deleteFD(int server_fd){
+	int n = close(data_table[(server_fd/-10) - 5].local_fd);
+    if(n < 0)
+    	return -1;
+
+    int i=(server_fd/ -10) - 5;
+    if(i < DATA_TABLE_SIZE && i >= 0){
+        data_table[i].local_fd = 0;
+        data_table[i].server_fd = 0;  
+        data_table[i].conn_mode = INVALID_FILE;        
+        data_table[i].flag = O_RDONLY;        
+        data_table[i].file_pwd[0] = '\0';     
+        return 0;
+    }
+    
+    errno = EBADF;
+    return -1;
+}
+
+int read(int file_descriptor, ssize_t nbyte, char *readBuffer){
+    int n = -1;
+    int i=(file_descriptor/ -10) - 5;
+   //printf("Makes it here netread\n");
+    //printf("LocalFD: %d\n", data_table[i].local_fd);
+    if(i<DATA_TABLE_SIZE && i>=0){
+        if(data_table[i].flag == O_WRONLY){
+            errno = EBADF;
+            return -1;
+        }
+        n = read(data_table[i].local_fd, readBuffer, (ssize_t)nbyte);
+        printf("return of read %d\n", n);
+        if(n >= 0){
+        	return n;
+        	printf("Makes it past read\n");
+        }
+    }
+    
+    errno = EBADF;
+    return -1;
+
+}
+
+int write(int file_descriptor, char * readBuffer, ssize_t nbyte){
+    int n = -1;
+	int i=(file_descriptor/ -10) - 5;
+    if(i < DATA_TABLE_SIZE && i >= 0){
+        if(data_table[i].flag==O_RDONLY){
+            errno = EBADF;
+            return -1;
+        }
+        n = write(data_table[i].local_fd, readBuffer,(ssize_t)nbyte);
+        printf("return of write%d\n", n);
+        if(n >= 0){
+        	return n;
+        	printf("Makes it past read\n");
+        }
+    }
+    
+    errno = EBADF;
+    return -1;
+}
+
+
+
+
