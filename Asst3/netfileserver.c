8004aa46f67b750e85f714399d05d6e5c9e17bf1
Neil Malu <Neil@nwk-141-209.nwk.ruw.rutgers.edu>
2017-05-01 20:03:53 -0400

    Check

--- Asst3/netfileserver.c
+++ Asst3/netfileserver.c
@@ -1,3 +1,18 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <string.h>
+#include <strings.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <signal.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include "libnetfiles.h"
 
 static int check = 0;
